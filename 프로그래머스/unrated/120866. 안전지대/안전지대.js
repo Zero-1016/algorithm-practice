35a5d1266c980a62111197fce136e16cc2e0d2d5
Jihyeong00 <iolpd42@gmail.com>
2023-12-15 18:47:52 +0900

    [level unrated] Title: 안전지대, Time: 0.41 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120866. 안전지대/안전지대.js
+++ 프로그래머스/unrated/120866. 안전지대/안전지대.js
@@ -1,37 +1,4 @@
 function solution(board) {
-    let changeBoard = JSON.parse(JSON.stringify(board))
-    board.forEach((el, y) => {
-        el.forEach((le, x) => {
-            if (le === 1) {
-                // 위
-                if (y !== 0) {
-                    changeBoard[y - 1][x] = 1
-                    if (x !== 0) {
-                        changeBoard[y - 1][x - 1] = 1
-                    }
-                    if (x !== el.length - 1) {
-                        changeBoard[y - 1][x + 1] = 1
-                    }
-                }
-                // 중간
-                if (x !== 0) {
-                    changeBoard[y][x - 1] = 1
-                }
-                if (x !== el.length - 1) {
-                    changeBoard[y][x + 1] = 1
-                }
-                // 아래
-                if (y !== changeBoard.length - 1) {
-                    changeBoard[y + 1][x] = 1
-                    if (x !== 0) {
-                        changeBoard[y + 1][x - 1] = 1
-                    }
-                    if (x !== el.length - 1) {
-                        changeBoard[y + 1][x + 1] = 1
-                    }
-                }
-            }
-        })
-    });
-    return changeBoard.flat().filter(v => v !== 1).length;
+    var answer = 0;
+    return answer;
 }
