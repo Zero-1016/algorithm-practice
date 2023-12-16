b836a5d565d7e8b7f510d8c156a37df2b8d24dd1
Jihyeong00 <iolpd42@gmail.com>
2023-12-16 10:51:07 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    const fib = new Array();
+    fib[0] = 1;
+    fib[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        fib[i] = (fib[i - 1] + fib[i - 2]) % 1234567;
+    }
+    return fib[n];
+}
