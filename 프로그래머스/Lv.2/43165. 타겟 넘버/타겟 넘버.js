03961ff6a383a10656a3ae592619b86e4d474014
Zero <iolpd42@gmail.com>
2024-01-11 18:05:39 +0900

    [level Lv.2] Title: 타겟 넘버, Time: 1.05 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,15 @@
+function solution(numbers, target) {
+    var answer = 0;
+    function targetNumber(array, index, value, target) {
+        if (index === array.length) {
+            if (target === value) answer++;
+            return;
+        }
+
+        targetNumber(array, index + 1, (value + array[index]), target);
+        targetNumber(array, index + 1, (value - array[index]), target);
+    }
+
+    targetNumber(numbers, 0, 0, target);
+    return answer;
+}
