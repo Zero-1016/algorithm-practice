748eea6a86d81a4dd2d65038fad57fbbc08182f7
Zero <iolpd42@gmail.com>
2024-03-11 02:17:08 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,25 +1,24 @@
 function solution(storey) {
     var answer = 0;
     while (storey) {
-        const one = storey % 10;
-        storey = Math.floor(storey / 10);
-        if (one === 0) {
-            continue
-        }
-        else if (one > 5) {
-            storey += 1
-            answer += 10 - one
+        const one = storey % 10; 
+        storey = Math.floor(storey / 10); 
+
+        if (one > 5) {
+            storey += 1;
+            answer += 10 - one; 
         }
         else if (one === 5) {
-            if (Math.round(storey % 10 / 10) === 1) {
-                storey += 1
-                answer += 5
+            const next = storey % 10;
+            if (next === 4 || next === 6) {
+                storey += 1;
+                answer += (next === 4 ? 6 : 4);
             } else {
-                answer += 5
+                answer += 5;
             }
         }
         else {
-            answer += one
+            answer += one;
         }
     }
     return answer;
