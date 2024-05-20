fd24f453cf5f099b3fd1a8b71140741dd98e47e9
Zero <iolpd42@gmail.com>
2024-05-20 23:08:55 +0900

    [level 2] Title: 전화번호 목록, Time: 241.18 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,13 +1,15 @@
 function solution(phone_book) {
-    var answer = true;
-    let count = 0;
-    let sort_array = phone_book.sort()
+    const hash = new Set(phone_book);
 
-    if (new Set(phone_book).size !== sort_array.length) return false
-
-    while (answer && count < phone_book.length - 1) {
-        if (sort_array[count + 1].substring(0, sort_array[count].length)?.includes(sort_array[count])) return false
-        count++;
+    for (let phone_number of hash) {
+        let prefix = "";
+        for (let char of phone_number) {
+            prefix += char
+            if (hash.has(prefix) && prefix !== phone_number) {
+                return false
+            }
+        }
     }
-    return answer;
+
+    return true;
 }
