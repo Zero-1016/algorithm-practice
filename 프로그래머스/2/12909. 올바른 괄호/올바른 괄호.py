28e9ea964ea7ebfffe0e95fa4d125fba34bf5f77
Zero <iolpd42@gmail.com>
2024-05-23 22:28:53 +0900

    [level 2] Title: 올바른 괄호, Time: 10.73 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = True
+    open = 0
+    close = 0
+
+    for i in range(len(s)):
+        if s[i] == "(":
+            open += 1
+        elif s[i] == ")" and open > close:
+            close += 1
+        else:
+            answer = False
+            break
+
+    if open != close:
+        answer = False
+
+    return answer
