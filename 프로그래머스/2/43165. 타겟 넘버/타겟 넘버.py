9de06293f1b51c159da8e8421acb7676d07dab84
Zero <iolpd42@gmail.com>
2024-05-30 22:02:04 +0900

    [level 2] Title: 타겟 넘버, Time: 2.81 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,15 @@
 def solution(numbers, target):
     answer = [0]
 
-    def dfs(value, index, numbers, target):
+    def dfs(value, index):
         if index == len(numbers):
             if value == target:
                 answer[0] += 1
             return
 
-        dfs(value + numbers[index], index + 1, numbers, target)
-        dfs(value - numbers[index], index + 1, numbers, target)
+        dfs(value + numbers[index], index + 1)
+        dfs(value - numbers[index], index + 1)
 
-    dfs(0, 0, numbers, target)
+    dfs(0, 0)
 
     return answer[0]
