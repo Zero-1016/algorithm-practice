980f33c44cc47e443bf4f42a29a232a052fdcd7c
Zero <iolpd42@gmail.com>
2024-07-25 21:57:24 +0900

    [level 2] Title: 주식가격, Time: 40.05 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,40 @@
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+function solution(prices) {
+  const answer = new Array(prices.length).fill(0);
+  const stack = [];
+  let length = prices.length;
+
+  for(let i = 0; i < length; i++) {
+    while(stack.length && prices[i] < prices[stack[stack.length - 1]]) {
+      let temp = stack.pop();
+      answer[temp] = i - temp;
+    }
+    stack.push(i);
+  }
+
+  while(stack.length) {
+    let temp = stack.pop();
+    answer[temp] = length - temp - 1;
+  }
+
+  return answer;
+}
