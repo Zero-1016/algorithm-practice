648088ece3ff2b0fc37a992507742a2254ca7a2d
Zero <iolpd42@gmail.com>
2024-07-25 20:23:29 +0900

    [level 2] Title: 괄호 회전하기, Time: 25.26 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,4 +1,42 @@
 function solution(s) {
-    var answer = -1;
+    let answer = 0;
+
+    const lastIndex = s.length - 1;
+
+    const bracket = {
+        "{": "}",
+        "(": ")",
+        "[": "]"
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let stack = [];
+        let isCorrect = true;
+
+        for (let j = 0; j < s.length; j++) {
+            const item = s[j]
+            if (bracket.hasOwnProperty(item)) {
+                stack.push(item);
+            } else {
+                const openBracket = stack.pop();
+                if (bracket[openBracket] !== item) {
+                    isCorrect = false;
+                    break;
+                }
+            }
+
+            if(lastIndex === j) {
+                if(stack.length !== 0) {
+                    isCorrect = false;
+                }
+            }
+        }
+        if (isCorrect) {
+            answer += 1;
+        }
+
+        s = s.slice(1) + s[0];
+    }
+
     return answer;
 }
