ba50df40d84e10324df1b7d19c137406b044c33c
Zero <iolpd42@gmail.com>
2024-07-30 19:22:11 +0900

    [level 2] Title: 할인 행사, Time: 751.70 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,18 +1,43 @@
-function solution(want, number, discount) {
-    var answer = 0;
-    let wants = []
-    want.forEach((value, index) => {
-        for (let i = 0; i < number[index]; i++) {
-            wants.push(value)
-        }
-    })
-    wants.sort()
+function isShallowEqual(object1, object2) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
 
-    discount.forEach((_, index) => {
-        const TodayDiscount = discount.slice(index, index + 10);
-        if (TodayDiscount.length !== wants.length) return;
-        TodayDiscount.sort()
-        if (TodayDiscount.toString() == wants.toString()) answer++
-    });
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (const key of keys1) {
+        if (object1[key] !== object2[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function solution(want, number, discount) {
+    const wantObj = {};
+    for (let i = 0; i < want.length; i++) {
+        wantObj[want[i]] = number[i];
+    }
+
+    let answer = 0;
+
+    for (let i = 0; i <= discount.length - 10; i++) {
+        const discount10d = {};
+
+        for (let j = i; j < i + 10; j++) {
+            if (discount10d[discount[j]] !== undefined) {
+                discount10d[discount[j]] += 1;
+            } else {
+                discount10d[discount[j]] = 1;
+            }
+        }
+
+        if (isShallowEqual(discount10d, wantObj)) {
+            answer += 1;
+        }
+    }
+
     return answer;
 }
