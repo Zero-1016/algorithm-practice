ce48cf239696f6ff9d13fdf9093c24d3443201ff
Zero <iolpd42@gmail.com>
2024-07-31 12:43:49 +0900

    [level 1] Title: 신고 결과 받기, Time: 214.71 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,30 +1,22 @@
 function solution(id_list, report, k) {
-    const report_list = {}
-    const count = {}
+    var answer = new Array(id_list.length).fill(0);
+    const noRepeatReport = new Set(report);
+    const userReportList = new Map(id_list.map((v) => [v, []]));
 
-    for (const reported of [...new Set(report)]) {
-        const [ban_user, report_user] = reported.split(' ')
-        if (report_list.hasOwnProperty(report_user)) {
-            report_list[report_user].push(ban_user)
-        } else {
-            report_list[report_user] = [ban_user]
-        }
+    for (let i of noRepeatReport) {
+        const [a, b] = i.split(' ');
+        const prev = userReportList.get(b);
+        userReportList.set(b, [...prev, a]);
     }
 
-    for (const report_user of Object.keys(report_list)) {
-
-        if (report_list[report_user].length >= k) {
-            for (const uid of report_list[report_user]) {
-                count[uid] = (count[uid] || 0) + 1
+    for (let i of userReportList) {
+        if (i[1].length >= k) {
+            const reportTargetIdIndex = id_list.findIndex((value) => value === i[0]);
+            for (let j of i[1]) {
+                const reportUserIdIndex = id_list.findIndex((value) => value === j);
+                answer[reportUserIdIndex]++;
             }
         }
     }
-
-    var answer = [];
-
-    for (const uid of id_list) {
-        answer.push(count[uid] || 0)
-    }
-
     return answer;
 }
