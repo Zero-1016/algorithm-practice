2a264c01821ddab46f1232cc44b5626f10a50e06
Zero <iolpd42@gmail.com>
2024-08-01 17:10:29 +0900

    [level 2] Title: 예상 대진표, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,29 +1,10 @@
-function solution(n, a, b) {
+function solution(n,a,b){
     var answer = 0;
-    let users = new Array(n).fill('').map((v, i) => {
-        if (i === a - 1 | i === b - 1) {
-            return 1
-        }
-        return 0
-    })
 
-    while (true) {
-        if (users.includes(2)) {
-            break;
-        }
-        let winner = []
-        let count = 0;
-        while (count < users.length / 2) {
-            if (users[count * 2] + users[count * 2 + 1] === 2) {
-                winner.push(users[count * 2] + users[count * 2 + 1])
-                break;
-            } else {
-                winner.push(users[count * 2] + users[count * 2 + 1])
-            }
-            count++;
-        }
-        users = winner
-        answer++
+    while (a !== b) {
+        a = Math.ceil( a / 2);
+        b = Math.ceil( b / 2);
+        answer += 1
     }
     return answer;
 }
