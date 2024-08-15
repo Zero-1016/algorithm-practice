45322b42ca08537412c275b1dcb858292c2ee6c2
Zero <iolpd42@gmail.com>
2024-08-15 16:08:20 +0900

    [level 2] Title: 롤케이크 자르기, Time: 168.75 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,24 +1,25 @@
 function solution(topping) {
-    let count = 0
-    let left = [];
-    let right = [];
-    const reTopping = [...topping].reverse()
-    let leftTemp = new Set();
-    let rightTemp = new Set();
-    for (let i = 0; i < topping.length - 1; i++) {
-        leftTemp.add(topping[i])
-        rightTemp.add(reTopping[i])
-        left.push(leftTemp.size)
-        right.push(rightTemp.size)
+    let splitCount = 0
+    
+    const toppingCounter = new Map();
+    for (const item of topping) {
+        toppingCounter.set(item, (toppingCounter.get(item) || 0) + 1)
     }
-
-    right.reverse();
-
-    for (let i = 0; i < right.length; i++) {
-        if (left[i] === right[i]) {
-            count++;
+    
+    const halfToppingSet = new Set();
+    
+    for (const item of topping) {
+        halfToppingSet.add(item);
+        toppingCounter.set(item, toppingCounter.get(item) - 1);
+        
+        if (toppingCounter.get(item) === 0) {
+            toppingCounter.delete(item);
+        }   
+    
+        if (halfToppingSet.size === toppingCounter.size){
+            splitCount += 1
         }
     }
-
-    return count
+    
+    return splitCount
 }
