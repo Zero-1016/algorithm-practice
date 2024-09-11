5f46d755da10331ad167f9aca0391ac2af845391
Zero <iolpd42@gmail.com>
2024-09-11 16:15:08 +0900

    Create 2022. Convert 1D Array Into 2D Array.js

---
+++ leetcode/Medium/2022. Convert 1D Array Into 2D Array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+var construct2DArray = function(original, m, n) {
+    if(m * n !== original.length) return []
+    const result = []
+    for(let i = 0; i < m; i++) {
+        result.push(original.slice(i * n, i * n + n))
+    }
+    return result
+};
