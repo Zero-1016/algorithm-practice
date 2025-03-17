95f5d1df5e0f8736bef62f938cacf13e675eb069
Jihyeong <iolpd42@gmail.com>
2025-03-17 21:57:39 +0900

    Create 24. Swap Nodes in Pairs.js

---
+++ leetcode/Medium/24. Swap Nodes in Pairs.js
@@ -0,0 +1,24 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  if (!head || !head.next) return head;
+
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let prev = dummy;
+
+  while (prev.next && prev.next.next) {
+    let first = prev.next;
+    let second = first.next;
+
+    first.next = second.next;
+    second.next = first;
+    prev.next = second;
+
+    prev = first;
+  }
+
+  return dummy.next;
+};
